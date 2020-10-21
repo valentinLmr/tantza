af9941051702ff14508c817d970393746adf2bc2
valentin lemaire <valentinlmr28@gmail.com>
2020-10-21 10:26:23 +0200

    add galeie section and implement design of photos

--- src/App.js
+++ src/App.js
@@ -10,8 +10,52 @@ function App() {
       </div>
     </section>
 
-    <section id='homePage'>
+    <section id='galerie'>
+      <div class='container-photo'>
+        <img class='photos' id='photo-left' src='/images/photo2.png' alt='photo1'></img>
+        <img class='photos' id='photo-center'src='/images/photo3.png' alt='photo1'></img>
+        <img class='photos' id='photo-right' src='/images/photo4.png' alt='photo1'></img>
+      </div>
 
+      <div class='container-photo'>
+      <img class='photos' id='photo-left' src='/images/photo5.png' alt='photo1'></img>
+      <img class='photos' id='photo-center'src='/images/photo6.png' alt='photo1'></img>
+      <img class='photos' id='photo-right' src='/images/photo1.png' alt='photo1'></img>
+      </div>
+      
+    </section>
+
+    <section id='menu'>
+      <div class='menu-listing'>
+        <h2>Starters</h2>
+        <ul class='starters-list'>
+          <li class='flex line'>
+            <p class='name'></p><p class='price'></p>
+          </li>
+        </ul>
+        <h2>Main Courses</h2>
+        <ul class='main-courses-list'>
+          <li class='flex line'>
+            <p class='name'></p><p class='price'></p>
+          </li>
+        </ul>
+          <h2> Desert</h2>
+        <ul class='desserts-list'>
+          <li class='flex line'>
+            <p class='name'></p><p class='price'></p>
+          </li>
+        </ul>
+          <h2> Drinks</h2>
+        <ul class='drinks-list'>
+          <li class='flex line'>
+            <p class='name'></p><p class='price'></p>
+          </li>
+        </ul>
+    
+      </div>
+
+      
+      
     </section>
 </div>
   );
