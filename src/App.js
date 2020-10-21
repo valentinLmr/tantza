972c4bf362055c6ea5c26864ffa558632149bde6
valentin lemaire <valentinlmr28@gmail.com>
2020-10-21 17:49:03 +0200

    nothing change

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
     </div>
     <section id='banner'>
       <div id="content-banner">
-      <h1 id='banner-title-brand'>Tantza</h1>
+      <h1 id='banner-title-brand'>Tzantza</h1>
       <p>58, rue Jean - Jacques Rousseau</p><p> 75001 - Paris</p>
       </div>
     </section>
